ce90cd6865189f5d9aed2f1a78e041dac7ef5783
chaptone <rakkan.j@gmail.com>
2020-12-04 17:59:44 +0700

    Fix Dropdown

--- src/ components/Dropdown.js
+++ src/ components/Dropdown.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Input, Select, Layout } from 'antd';
+import { Select, Layout } from 'antd'
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
@@ -7,13 +7,19 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       return null
     }
     return (
-      <Select.Option value={option.value}>{option.label}</Select.Option>
+      <Select.Option value={option.value} title={option.label}>
+        {option.label}
+      </Select.Option>
     )
   })
 
   return (
     <Layout>
-      <Select defaultValue="" value={selected.label} onChange={(v) => onSelectedChange(v)}>
+      <Select
+        defaultValue=""
+        value={selected.label}
+        onSelect={(v, o) => onSelectedChange({ label: o.title, value: v })}
+      >
         {renderedOptions}
       </Select>
     </Layout>
